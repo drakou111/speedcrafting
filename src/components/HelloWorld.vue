baa57c695cac34c0c34e91d3c5b410bb871a96b8
Drakou111 <2133123@csfoy.ca>
2023-04-22 07:01:52 -0400

    temp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>Fun!</h1>
+  <div>
+    <button @click="incrementValue">Increment</button>
+    <p>Current value: {{ counterValue }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, runTransaction } from "firebase/database";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const firebaseConfig = {
+  apiKey: "AIzaSyBeRVHBP909P1fJxAJW6PMpyvBU9bOqs3o",
+  authDomain: "speedcrafting-e8421.firebaseapp.com",
+  projectId: "speedcrafting-e8421",
+  storageBucket: "speedcrafting-e8421.appspot.com",
+  messagingSenderId: "633410195096",
+  appId: "1:633410195096:web:508221c211df68ec678aed",
+  measurementId: "G-P3JQJW3NRN",
+  databaseURL: "https://speedcrafting-e8421-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default {
+  data() {
+    return {
+      counterValue: 0,
+    };
+  },
+  methods: {
+    async incrementValue() {
+      const valueRef = ref(database, "counter");
+
+      // Read the current value from the database and log it to the console
+      onValue(valueRef, (snapshot) => {
+        this.counterValue = snapshot.val();
+        console.log("Current value:", this.counterValue);
+      });
+
+      // Use a transaction to atomically increment the value
+      try {
+        await runTransaction(valueRef, (currentValue) => {
+          return (currentValue || 0) + 1;
+        });
+        console.log("Value incremented successfully");
+      } catch (error) {
+        console.log("Error incrementing value:", error);
+      }
+    },
+  },
+  mounted() {
+    const valueRef = ref(database, "counter");
+
+    onValue(valueRef, (snapshot) => {
+      this.counterValue = snapshot.val();
+      console.log("Current value:", this.counterValue);
+    });
+  },
+};
+</script>
